f87474243ac28e4769db805628f517ce06b46872
Lê Nguyễn Quốc Huy <huynq2.21it@vku.udn.vn>
2026-03-10 11:21:35 +0700

    build full UI home website

--- frontend_Ecom_User/e-commerce-interface/app/page.tsx
+++ frontend_Ecom_User/e-commerce-interface/app/page.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/header";
 import { SearchFilter } from "./components/search_filter";
 import MainSection from "./components/main_section";
 import FoodCategory from "./components/FoodCategory";
-import SpecialOffer from "./components/SpecialOffer";
+import FoodEvaluation from "./components/foodevaluation";
 import { Footer } from "./components/footer";
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
       <Header />
       <MainSection/>
       <FoodCategory/>
-      <SpecialOffer/>
+      <FoodEvaluation/>
       <Footer/>
 </main>
 
